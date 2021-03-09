74cc36e8c08de2d596f16ccc80eda6cde2e7571b
Yoonkyung Son <yoonison722@gomcorp.com>
2021-03-09 17:18:31 +0900

    셋팅?

---
+++ src/components/list.vue
@@ -0,0 +1,14 @@
+<template>
+ <div> 
+	 <button v-on:class="$style.hand"> 버튼 </button> 
+ </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.hand { cursor: pointer; background-color: #f5f5f5; color: #333333}
+</style>
